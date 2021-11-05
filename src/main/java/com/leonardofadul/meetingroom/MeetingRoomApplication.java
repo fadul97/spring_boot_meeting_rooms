dab1ead2c4ca35f5852b6e9e493c3834713495ac
Leonardo Fadul <lsffadul@gmail.com>
2021-11-05 10:48:17 -0300

    Instantiation operation

--- src/main/java/com/leonardofadul/meetingroom/MeetingRoomApplication.java
+++ src/main/java/com/leonardofadul/meetingroom/MeetingRoomApplication.java
@@ -1,13 +1,32 @@
 package com.leonardofadul.meetingroom;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.leonardofadul.meetingroom.domain.Company;
+import com.leonardofadul.meetingroom.repositories.CompanyRepository;
+
 @SpringBootApplication
-public class MeetingRoomApplication {
+public class MeetingRoomApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CompanyRepository companyRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeetingRoomApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Company c1 = new Company(1, "Company 1", "company1");
+		Company c2 = new Company(2, "Company 2", "company2");
+		Company c3 = new Company(3, "Company 3", "company3");
+		Company c4 = new Company(4, "Company 4", "company4");
+		
+		companyRepository.saveAll(Arrays.asList(c1, c2, c3, c4));	
+	}
 }
