fd59be2fdb3e68fb5bd1b371ed3f1c9c687ec0bb
Leonardo Fadul <lsffadul@gmail.com>
2021-11-18 10:54:40 -0300

    Delete Company with DELETE

--- src/main/java/com/leonardofadul/meetingroom/services/CompanyService.java
+++ src/main/java/com/leonardofadul/meetingroom/services/CompanyService.java
@@ -3,9 +3,11 @@ package com.leonardofadul.meetingroom.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.leonardofadul.meetingroom.domain.Company;
+import com.leonardofadul.meetingroom.exceptions.DataIntegrityException;
 import com.leonardofadul.meetingroom.exceptions.ObjectNotFoundException;
 import com.leonardofadul.meetingroom.repositories.CompanyRepository;
 
@@ -28,5 +30,14 @@ public class CompanyService {
 	public Company update(Company obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try{
+	        repo.deleteById(id);
+	    } catch (DataIntegrityViolationException e) {
+	    	throw new DataIntegrityException("It is not possible to delete a company that still has rooms to rent.");
+	    }
 	}
 }
